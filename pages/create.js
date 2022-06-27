f3e8594c380a48f265c5984f4f3bd75a2a95f3bf
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2022-06-27 21:19:38 +0800

    update front

--- pages/create.js
+++ pages/create.js
@@ -3,24 +3,17 @@ export default function Create(){
         <div className="container pt-3">               
             <div className="card mb-3 mt-5">
                 <div className="card-body">
-                    <h5 className="mb-3">Write to us</h5>
+                    <h5 className="mb-3">Write to Testimonal</h5>
                     <div className="contact-form">
                         <form action="#">
                             <div className="form-group mb-3">
                                 <input className="form-control" type="text" placeholder="Your name"/>
                             </div>
                             <div className="form-group mb-3">
-                                <input className="form-control" type="email" placeholder="Enter email"/>
+                                <input className="form-control" type="text" placeholder="Comment"/>
                             </div>
                             <div className="form-group mb-3">
-                                <select className="form-select" name="topic">
-                                    <option value="">Authors Help</option>
-                                    <option value="">Buyer Help</option>
-                                    <option value="">Licenses</option>
-                                </select>
-                            </div>
-                            <div className="form-group mb-3">
-                                <textarea className="form-control" name="textarea" cols="30" rows="10" placeholder="Write details"></textarea>
+                                <input className="form-control" min={1} max={5} type="number" placeholder="1-5"/>
                             </div>
                             <button className="btn btn-primary w-100">Send Now</button>
                         </form>
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,31 @@
+import Link from "next/link";
 
 export default function Home(){
   return(
     <div>
       <div className="pt-5 mt-2"></div>
       <div className="container">
-        <div className="card card-bg-img bg-img bg-overlay mb-3">
-          <div className="card-body direction-rtl p-5">
-            <h2 className="text-white">Reusable elements</h2>
-            <p className="mb-4 text-white">More than 220+ reusable modern design elements. Just copy the code and paste it on
-              your desired page.</p><a className="btn btn-warning" href="elements.html">All elements</a>
-          </div>
-        </div>
-      </div>
-      <div className="container">
-        <div className="card bg-primary mb-3 bg-img">
-          <div className="card-body direction-rtl p-5">
-            <h2 className="text-white">Ready pages</h2>
-            <p className="mb-4 text-white">Already designed more than 100+ pages for your needs. Such as - Authentication,
-              Chats, eCommerce, Blog &amp; Miscellaneous pages.</p><a className="btn btn-warning" href="pages.html">All
-              pages</a>
+        <div class="card single-product-card">
+          <div class="card-body">
+            <div class="d-flex align-items-center">
+              <div className="row">
+                <div className="col-10">
+                  <div class="card-content">
+                    <a class="product-title d-block text-truncate mt-0" href="page-shop-details.html">Kasmira</a>
+                    <p class="sale-price">Yap layanan ini sangat bagus dan sangat terjamin</p>
+                  </div>
+                </div>
+                <div className="col-2 text-end align-self-center">
+                  <i className="bi bi-star-fill text-yellow"/>
+                  <span className="d-block"/>
+                  <Link href={'/about'}>
+                    <a>
+                      <i className="bi bi-arrow-bar-right d-inline-block"/><span className="small-text align-middle">Detail</span>
+                    </a>
+                  </Link>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
